9736cfc0e1a88fa385b270bd3b40c8035e1928b9
gastonant <antoine.gaston@rte-france.com>
2020-12-09 08:59:29 +0100

    - refactoring après intégration du strike

--- bowling.py
+++ bowling.py
@@ -3,7 +3,7 @@ class Scorer(object):
     score = 0
     score_frame = 0
     frame = 1
-    lancer = 1
+    lancer = 0
     facteur_spare = 1
     facteur_strike = 1
 
@@ -12,21 +12,29 @@ class Scorer(object):
 
     def effectuer_un_lancer(self, nb_quilles_tombees=0):
         self.score += self.facteur_spare*self.facteur_strike*nb_quilles_tombees
-        self.facteur_spare = 1
         self.score_frame += nb_quilles_tombees
         self.lancer += 1
-        if self.lancer > 2:
-            if self.score_frame == 10:
+        self._gerer_spare()
+        self._gerer_strike()
+
+    def _gerer_spare(self):
+        self.facteur_spare = 1
+        if self.lancer > 1: # Deuxième lancer
+            if self.score_frame == 10: # Spare
                 self.facteur_spare = 2
-            if self.facteur_strike == 2:
-                self.facteur_strike = 1
             self.passer_au_frame_suivant()
-        else:
-            if self.score_frame == 10:
+
+    def _gerer_strike(self):
+        if self.lancer == 1: # Premier lancer
+            if self.score_frame == 10: # Strike
                 self.facteur_strike = 2
                 self.passer_au_frame_suivant()
+        else: # Deuxième lancer
+            if self.facteur_strike == 2: # Strike au frame précédent
+                self.facteur_strike = 1
+            self.passer_au_frame_suivant()
 
     def passer_au_frame_suivant(self):
         self.frame += 1
-        self.lancer = 1
+        self.lancer = 0
         self.score_frame = 0
